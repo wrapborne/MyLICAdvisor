7d6b308f78947399f8917f657b5d91d9326bd3d7
wrapborne <vip131995@gmail.com>
2025-07-03 00:51:12 +0530

    Update AdminDashboard.jsx

--- frontend/components/AdminDashboard.jsx
+++ frontend/components/AdminDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "../axiosConfig";
+
 
 const AdminDashboard = () => {
   const [plans, setPlans] = useState([]);
