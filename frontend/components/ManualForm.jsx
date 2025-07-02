d42a4cb919bf94f3de1aaf7dec7ec299fcab715b
wrapborne <vip131995@gmail.com>
2025-07-03 00:50:41 +0530

    Update ManualForm.jsx

--- frontend/components/ManualForm.jsx
+++ frontend/components/ManualForm.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "../axiosConfig";
 
 const ManualForm = () => {
   const [schema, setSchema] = useState([]);
